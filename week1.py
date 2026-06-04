f198103830f87d96d037ddc7fc341dbd402af072
Neji <kalambaha@gmail.com>
2026-06-04 18:06:02 +0200

    feat: week2 chater classifier with 4 statuses

--- week1.py
+++ week1.py
@@ -1,7 +1,12 @@
 name = input("имя?")
-sold = int(input("сколько продаж?"))
-plan = int(input("план продаж?"))
-if sold < plan:
-    print("ПЛАН не выполнен!")
-elif sold >= plan:
-    print (f"План выполнен  {name}:  {(sold/plan)*100}" , "%")
+sold = int(input("продаж?"))
+time = int(input("time?"))
+error = int(input("errors?"))
+if sold>=15 and time <=5 and error==0:
+    print (f"{name}- status TOP")
+elif sold>=10 and time <=10 and error <= 1:
+    print (f"{name}- status GOOD")
+elif sold>=5 and error <= 3:
+    print (f"{name}- status NORMA")
+else:
+    print(f"{name}- status Trouble")
